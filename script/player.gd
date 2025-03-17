6f6ba3061604a00b765e0168b715936a95d0a764
Loltaku <gongzhudianxiammd@163.com>
2025-03-18 01:29:02 +0800

    速度100加速度70摩擦力2000

--- script/player.gd
+++ script/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const ACCELERATION = 800.0  # 新增加速度
-const FRICTION = 1000.0     # 新增摩擦力
+const SPEED = 100.0
+const ACCELERATION = 70.0  # 新增加速度
+const FRICTION = 2000.0     # 新增摩擦力
 const JUMP_VELOCITY = -239.0
 
 
@@ -19,9 +19,10 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	 # 使用加速度更平滑
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
 	move_and_slide()
